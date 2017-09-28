8deded01c6df0e06b38f5bf3431ec880b2030b27
GavinThomas1192 <gthomas1192@gmail.com>
2017-09-28 09:08:58 -0700

    added front faving website, Not live

--- model/user.js
+++ model/user.js
@@ -75,3 +75,17 @@ User.methods.generateToken = function() {
 };
 
 module.exports = mongoose.model('user', User);
+
+
+
+function tick() { 
+  const element = (
+    <div>
+    <h1>Hello, world!</h1>
+    <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root') );
+  } setInterval(tick, 1000);
